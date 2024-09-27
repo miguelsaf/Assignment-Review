086749fa9fe6352997d00b52777a7af621fc7d82
miguelsaf <miguelarcanjo03@gmail.com>
2024-09-27 22:25:19 +0100

    Move src folder

---
+++ src/main/java/com/hcc/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.hcc.config;
+
+import com.hcc.filters.jwtFilter;
+import com.hcc.services.UserDetailServiceImpl;
+import com.hcc.utils.CustomPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailServiceImpl userDetailServiceImpl;
+
+    @Autowired
+    CustomPasswordEncoder customPasswordEncoder;
+
+    @Autowired
+    jwtFilter jwtFilt;
+
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(customPasswordEncoder.getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().and(); // do not dissable this lot here just for now!!
+
+        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+
+        http = http.exceptionHandling().authenticationEntryPoint((request, response, exception) -> {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+        }).and();
+
+        http.authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore(jwtFilt, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
